d390e565530eb93a9a7f65932c46097655e99b24
rosihanwar <ahmadrosihan@student.ub.ac.id>
2024-12-03 17:19:51 +0700

    Update manauangmu.java

--- manauangmu.java
+++ manauangmu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tugasakhir {
+public class manauangmu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
